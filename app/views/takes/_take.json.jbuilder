ce672ef5843edd89f3dd0a64fa1b49f369121a1a
gyeol1212 <gyeol1212@likelion.org>
2018-06-30 15:59:07 +0900

    scaffold

---
+++ app/views/takes/_take.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! take, :id, :title, :content, :created_at, :updated_at
+json.url take_url(take, format: :json)
---
+++ db/migrate/20180630064757_create_takes.rb
@@ -0,0 +1,10 @@
+class CreateTakes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :takes do |t|
+      t.string :title
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end

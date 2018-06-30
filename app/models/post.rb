5ebffede4a27768113a883bec7b84f74fbafd7c2
gyeol1212 <gyeol1212@likelion.org>
2018-06-30 22:29:23 +0900

    aws

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ApplicationRecord
     has_many :post_comments, dependent: :destroy
+   
+    mount_uploader :image, S3Uploader
+       
+    def self.search(search)
+        where("title || content LIKE ?", "%#{search}%") 
+    end
+
 end
